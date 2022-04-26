c8de93bc51446f9931db62b0bb581dc35d89682e
Sacred-Pixel <tskritz@gmail.com>
2022-04-26 09:43:12 -0400

    a timer to count down your reps

---
+++ repInterval.py
@@ -0,0 +1,18 @@
+# repInterval.py
+
+from microbit import *
+
+while True:
+    seconds = int(input("Enter a time in seconds: "))
+    reps = int(input("Enter a number of reps: "))
+    
+    while(reps > 0):
+        counter=seconds
+        print("Reps left: "+ str(reps) + "\n")
+        
+        while(counter>=0):
+            print(str(counter))
+            counter-=1
+            sleep(1000)
+        reps-=1
+        print("\n")
